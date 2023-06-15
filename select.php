558a87b0b753a4bc38f0b96129758d72c2bb6662
Zakariaismeusername <133658166+Zakariaismeusername@users.noreply.github.com>
2023-06-15 15:03:47 +0200

    Add files via upload

---
+++ select.php
@@ -0,0 +1,34 @@
+<?php
+$host = 'localhost:3307';
+$db   = 'winkel';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "Connectie gemaakt!";
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+Connected to database (winkel).
+?>
+
+<?php
+// Door de parameters te gebruiken.
+$sql = "SELECT * FROM producten";
+// Door die single row te selecteren met die code.
+$sql = "select product_code FROM producten";
+// Deze weet ik niet 
+$sql = "select product_code FROM producten";
+
+?>
